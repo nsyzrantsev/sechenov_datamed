7d92fafdd8a95a88778224ec0245dde5a19bc975
Nikita <nicksyz13@gmail.com>
2021-11-04 13:57:56 +0300

    Index function is updated. Render functionality of table is added.

--- datamed/search_engine/views.py
+++ datamed/search_engine/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    query = request.GET.get('q')
+    if request.method == "GET":
+        if query is not None:
+            query = request.GET.get('q')
+            db_objs = DdiXFact.objects.filter(sentence_txt__icontains=query)
+            data_list = list(db_objs.values())
+            return render(request, 'search_engine/index.html', {'data_list': data_list})
+    return render(request, 'search_engine/index.html', {})
