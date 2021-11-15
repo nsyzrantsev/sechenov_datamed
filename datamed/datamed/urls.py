0b37d61178494839a9fd414208ab2e820e6a1753
Nikita <nicksyz13@gmail.com>
2021-11-15 19:28:32 +0300

    text_checker is added

--- datamed/datamed/urls.py
+++ datamed/datamed/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('text_checker.urls')),
     path('', include('search_engine.urls')),
     path('admin/', admin.site.urls),
 ]
