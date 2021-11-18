6b0f331f30f36bc574a782f35a08278f46bd670d
Nikita <nicksyz13@gmail.com>
2021-11-18 14:02:21 +0300

    sys path to BERT model is added

--- datamed/text_checker/views.py
+++ datamed/text_checker/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import *
-import json
-from .services import *
+import sys
+sys.path.append('../datamed/BERT_core/')
+from BERT_core.main import main
 
 
 def index(request):
-    return render(request, 'text_checker/index.html', {})
+    prediction = {}
+    article_text = request.GET.get('text')
+    if request.method == "GET":
+        if article_text is not None:
+            prediction = main([article_text])
+    return render(request, 'text_checker/index.html', {'prediction': prediction})
