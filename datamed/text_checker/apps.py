b1bbf9880492461095c4623c529824d1168abdab
Nikita <nicksyz13@gmail.com>
2021-11-18 14:01:09 +0300

    App name is corrected

--- datamed/text_checker/apps.py
+++ datamed/text_checker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PubmedBertConfig(AppConfig):
+class TextCheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'text_checker'
