a8d93973c88cf0861cfa6d6dd531c7e237eaf242
Nikita <nicksyz13@gmail.com>
2021-11-18 13:59:30 +0300

    name argument in path() is updated to - search_page

--- datamed/search_engine/urls.py
+++ datamed/search_engine/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('search/', index, name='index'),
+    path('', index, name='search_page'),  # http://127.0.0.1:8000/
 ]
