95406882cecd570315a58019d1b4d35fcfa2914d
Nikita <nicksyz13@gmail.com>
2021-11-18 23:49:10 +0300

    Updated import token fun name

--- datamed/BERT_core/main.py
+++ datamed/BERT_core/main.py
@@ -4,6 +4,9 @@ from transformers import BertForTokenClassification, BertForSequenceClassificati
 from transformers import BertTokenizer
 from ner_bert import NerBert
 from re_bert import ReBert
+from os import path
+
+path_to_BERT = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'BERT_core')
 
 
 def main(texts):
@@ -13,7 +16,7 @@ def main(texts):
     initialized_re = re_initialization()
     NER_model = NerBert(*initialized_ner, batch_size)
     NER_tokens = NER_model.predict_by_bert_ner(sentences)
-    NER_sentences = NER_model.import_tokens(sentences, NER_tokens)
+    NER_sentences = NER_model.import_tokens_in_sentences(sentences, NER_tokens)
     RE_model = ReBert(*initialized_re, batch_size)
     RE_interactions = RE_model.predict_by_bert_re(NER_sentences)
     return result_list(sentences, NER_sentences, RE_interactions)
@@ -22,9 +25,11 @@ def main(texts):
 # NER-BERT initialization
 def ner_initialization():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = BertForTokenClassification.from_pretrained('../datamed/BERT_core/NER5')
+    path_to_bert_ner = str(path.join(path_to_BERT, 'NER5'))
+    model = BertForTokenClassification.from_pretrained(path_to_bert_ner)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    with open('../datamed/BERT_core/labels_dictionary.json') as json_file:
+    path_to_labels_dict = str(path.join(path_to_BERT, 'labels_dictionary.json'))
+    with open(path_to_labels_dict) as json_file:
         labels_dictionary = json.load(json_file)
     model.to(device)
     model.eval()
@@ -34,9 +39,12 @@ def ner_initialization():
 # RE-BERT initialization
 def re_initialization():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = BertForSequenceClassification.from_pretrained('../datamed/BERT_core/Bert_med_re_v5_cont_ep_3')
-    tokenizer = BertTokenizer.from_pretrained('../datamed/BERT_core/MEDtokenizer')
-    with open('../datamed/BERT_core/RElabels.json') as json_file:
+    path_to_bert_re = str(path.join(path_to_BERT, 'Bert_med_re_v5_cont_ep_3'))
+    model = BertForSequenceClassification.from_pretrained(path_to_bert_re)
+    path_to_medtokenizer = str(path.join(path_to_BERT,'MEDtokenizer'))
+    tokenizer = BertTokenizer.from_pretrained(path_to_medtokenizer)
+    path_to_relabels = str(path.join(path_to_BERT, 'RElabels.json'))
+    with open(path_to_relabels) as json_file:
         labels_dictionary = json.load(json_file)
     model.to(device)
     model.eval()
@@ -48,9 +56,9 @@ def result_list(sentences, NER_sentences, RE_interactions):
     answer_list = []
     for i, sentence in enumerate(sentences):
         sentence_dict = {
-            'Изначальный текст': sentence,
-            'После обработки BERT_core': NER_sentences[i],
-            'DDI': RE_interactions[i]
+            'text_before_bert': sentence,
+            'text_after_bert': NER_sentences[i],
+            'ddi': RE_interactions[i]
         }
         answer_list.append(sentence_dict)
     return answer_list
