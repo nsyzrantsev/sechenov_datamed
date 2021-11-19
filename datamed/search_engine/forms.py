1337b07808dcc8bdd26c373f1632e5b702acb24c
Nikita <nicksyz13@gmail.com>
2021-11-19 13:01:13 +0300

    Initial commit

---
+++ datamed/search_engine/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class AddSearchForm(forms.Form):
+    user_query = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите запрос...'
+        }))
