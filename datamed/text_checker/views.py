e7e7ed7c751cd66d91fba124e9b00580ffbe0a96
Nikita <nicksyz13@gmail.com>
2021-11-19 13:00:20 +0300

    Updated for forms

--- datamed/text_checker/views.py
+++ datamed/text_checker/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 import json
-
+from .forms import AddTextareaForm
 import sys
 from pathlib import Path
+
 path_to_bert = str(Path.joinpath(Path(__file__).resolve().parent.parent, 'BERT_core'))
 sys.path.append(path_to_bert)
 
@@ -10,9 +11,16 @@ from BERT_core.main import main
 
 
 def index(request):
-    prediction = {}
-    article_text = request.GET.get('text')
     if request.method == "GET":
-        if article_text is not None:
-            prediction = main([article_text])
-    return render(request, 'text_checker/text_area.html', {'title': 'Анализ текста', 'prediction': json.dumps(prediction)})
+        form = AddTextareaForm(request.GET)
+        if form.is_valid():
+            query = form['user_text'].value()
+            prediction = main([query])
+            return render(request, 'text_checker/text_area.html',
+                          {'form': form,
+                           'title': 'Анализ текста',
+                           'prediction': json.dumps(prediction)})
+    else:
+        form = AddTextareaForm()
+    return render(request, 'text_checker/text_area.html', {'form': form,
+                                                           'title': 'Анализ текста'})
