1542766b9d5b60166ec5161e94dee9e69c3daa60
Nikita <nicksyz13@gmail.com>
2021-11-19 18:55:35 +0300

    Views are changed to class based notation

--- datamed/search_engine/views.py
+++ datamed/search_engine/views.py
@@ -2,19 +2,25 @@ from django.shortcuts import render
 import json
 from .services import get_articles_from_db
 from .forms import AddSearchForm
+from django.views.generic import FormView
 
 
-def index(request):
-    if request.method == "GET":
-        form = AddSearchForm(request.GET)
-        if form.is_valid():
-            query = form['user_query'].value()
-            data_list = get_articles_from_db(query)
-            return render(request, 'search_engine/search_with_grid.html',
-                          {'form': form,
-                           'title': 'Поиск в базе данных',
-                           'db_data_list': json.dumps(data_list)})
-    else:
-        form = AddSearchForm()
-    return render(request, 'search_engine/search_with_grid.html', {'form': form,
-                                                                   'title': 'Поиск в базе данных'})
+class SearchView(FormView):
+    template_name = 'search_engine/search_with_grid.html'
+    search_form = AddSearchForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Поиск в базе данных'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            form = self.search_form(request.GET)
+            if form.is_valid():
+                query = form['user_query'].value()
+                data_list = get_articles_from_db(query)
+                return render(request, 'search_engine/search_with_grid.html',
+                              {'form': form,
+                               'db_data_list': json.dumps(data_list)})
+        return render(request, 'search_engine/search_with_grid.html', {'form': self.search_form})
--- datamed/text_checker/views.py
+++ datamed/text_checker/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
 import json
 from .forms import AddTextareaForm
+from django.views.generic import FormView
 import sys
 from pathlib import Path
 
 path_to_bert = str(Path.joinpath(Path(__file__).resolve().parent.parent, 'BERT_core'))
 sys.path.append(path_to_bert)
 
-from BERT_core.main import main
+from BERT_core.main import main as bert_prediction
 
 
-def index(request):
-    if request.method == "GET":
-        form = AddTextareaForm(request.GET)
-        if form.is_valid():
-            query = form['user_text'].value()
-            prediction = main([query])
-            return render(request, 'text_checker/text_area.html',
-                          {'form': form,
-                           'title': 'Анализ текста',
-                           'prediction': json.dumps(prediction)})
-    else:
-        form = AddTextareaForm()
-    return render(request, 'text_checker/text_area.html', {'form': form,
-                                                           'title': 'Анализ текста'})
+class TextareaView(FormView):
+    template_name = 'text_checker/text_area.html'
+    textarea_form = AddTextareaForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Анализ текста'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            form = self.textarea_form(request.GET)
+            if form.is_valid():
+                query = form['user_text'].value()
+                print(query)
+                prediction = bert_prediction([query])
+                return render(request, self.template_name, {'form': form, 'prediction': json.dumps(prediction)})
+        return render(request, self.template_name, {'form': self.textarea_form})
