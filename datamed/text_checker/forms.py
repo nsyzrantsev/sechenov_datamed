dbaf03764e4d170077fc09c4fee5d21ab5dd8955
Nikita <nicksyz13@gmail.com>
2021-11-19 13:01:27 +0300

    Initial commit

---
+++ datamed/text_checker/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class AddTextareaForm(forms.Form):
+    user_text = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Введите текст...'
+        }))
