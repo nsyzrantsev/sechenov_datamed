2ac810fa42330e78e9a00d58bd734a1917227918
Nikita <nicksyz13@gmail.com>
2021-11-21 19:18:41 +0300

    POST function is added

--- datamed/search_engine/views.py
+++ datamed/search_engine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import json
-from .services import get_articles_from_db
+from .services import get_articles_from_db, save_articles_in_db
 from .forms import AddSearchForm
 from django.views.generic import FormView
 
@@ -15,12 +15,22 @@ class SearchView(FormView):
         return context
 
     def get(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            form = self.search_form(request.GET)
-            if form.is_valid():
-                query = form['user_query'].value()
-                data_list = get_articles_from_db(query)
-                return render(request, 'search_engine/search_with_grid.html',
-                              {'form': form,
-                               'db_data_list': json.dumps(data_list)})
+        form = self.search_form(request.GET)
+        if form.is_valid():
+            query = form['user_query'].value()
+            data_list = get_articles_from_db(query)
+            return render(request, 'search_engine/search_with_grid.html',
+                          {'form': form,
+                           'db_data_list': json.dumps(data_list)})
+        return render(request, 'search_engine/search_with_grid.html', {'form': self.search_form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.search_form(request.POST)
+        if form.is_valid():
+            query = form['user_query'].value()
+            save_articles_in_db(query)
+            data_list = get_articles_from_db(query)
+            return render(request, 'search_engine/search_with_grid.html',
+                          {'form': form,
+                           'db_data_list': json.dumps(data_list)})
         return render(request, 'search_engine/search_with_grid.html', {'form': self.search_form})
