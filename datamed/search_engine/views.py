60104863f5f3755f18e5c33ef2cfcb0b3da96e41
Nikita <nicksyz13@gmail.com>
2021-11-22 15:26:09 +0300

    Local search view is added

--- datamed/search_engine/views.py
+++ datamed/search_engine/views.py
@@ -5,13 +5,13 @@ from .forms import AddSearchForm
 from django.views.generic import FormView
 
 
-class SearchView(FormView):
-    template_name = 'search_engine/search_with_grid.html'
+class SearchPubmedView(FormView):
+    template_name = 'search_engine/search_pubmed.html'
     search_form = AddSearchForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Поиск в базе данных'
+        context['title'] = 'Поиск в pubmed'
         return context
 
     def get(self, request, *args, **kwargs):
@@ -19,18 +19,43 @@ class SearchView(FormView):
         if form.is_valid():
             query = form['user_query'].value()
             data_list = get_articles_from_db(query)
-            return render(request, 'search_engine/search_with_grid.html',
+            return render(request, self.template_name,
                           {'form': form,
                            'db_data_list': json.dumps(data_list)})
-        return render(request, 'search_engine/search_with_grid.html', {'form': self.search_form})
+        return render(request, self.template_name, {'form': self.search_form})
 
     def post(self, request, *args, **kwargs):
         form = self.search_form(request.POST)
         if form.is_valid():
-            query = form['user_query'].value()
-            save_articles_in_db(query)
-            data_list = get_articles_from_db(query)
-            return render(request, 'search_engine/search_with_grid.html',
+            query_text = form['user_query'].value()
+            articles_number = form['articles_number'].value()
+            # Check is a number field is empty or not
+            if articles_number != '':
+                save_articles_in_db(query_text, int(articles_number))
+            else:
+                save_articles_in_db(query_text)
+            data_list = get_articles_from_db(query_text)
+            return render(request, self.template_name,
                           {'form': form,
                            'db_data_list': json.dumps(data_list)})
-        return render(request, 'search_engine/search_with_grid.html', {'form': self.search_form})
+        return render(request, self.template_name, {'form': self.search_form})
+
+
+class SearchLocalView(SearchPubmedView):
+    template_name = 'search_engine/search_local.html'
+    search_form = AddSearchForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Поиск в локальной базе данных'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        form = self.search_form(request.GET)
+        if form.is_valid():
+            query = form['user_query'].value()
+            data_list = get_articles_from_db(query)
+            return render(request, self.template_name,
+                          {'form': form,
+                           'db_data_list': json.dumps(data_list)})
+        return render(request, self.template_name, {'form': self.search_form})
