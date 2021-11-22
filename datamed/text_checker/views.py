d6703b11df7618bfdd41e4e4c0f7be2c7e2ec9be
Nikita <nicksyz13@gmail.com>
2021-11-22 15:29:24 +0300

    Variables names updated

--- datamed/text_checker/views.py
+++ datamed/text_checker/views.py
@@ -25,6 +25,6 @@ class TextareaView(FormView):
             form = self.textarea_form(request.GET)
             if form.is_valid():
                 query = form['user_text'].value()
-                prediction = bert_prediction([query])
+                prediction = bert_prediction(query)
                 return render(request, self.template_name, {'form': form, 'prediction': json.dumps(prediction)})
         return render(request, self.template_name, {'form': self.textarea_form})
