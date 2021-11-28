e0a74846f35dfb19fb876ed7ad1ec91a7c7fd3be
Nikita <nicksyz13@gmail.com>
2021-11-28 16:09:23 +0300

    Number form is deleted

--- datamed/search_engine/forms.py
+++ datamed/search_engine/forms.py
@@ -3,15 +3,10 @@ from django import forms
 
 class AddSearchForm(forms.Form):
     user_query = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите запрос...'
-        }))
-    articles_number = forms.IntegerField(
-        min_value=0,
-        max_value=10,
-        required=False,
-        widget=forms.NumberInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите число статей...'
-        }))
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите запрос...'
+            },
+        ),
+    )
